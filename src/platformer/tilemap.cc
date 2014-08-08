f497a7a750403e26fab03bd6571e52d9b18ce3e4
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-08-08 20:00:49 -0300

    Should draw but does not.

--- src/platformer/tilemap.cc
+++ src/platformer/tilemap.cc
@@ -22,16 +22,26 @@ using ugdk::graphic::Primitive;
 using ugdk::graphic::VertexData;
 using ugdk::graphic::opengl::ShaderUse;
 using ugdk::graphic::manager;
+using ugdk::graphic::opengl::VertexType;
 
 namespace {
 
 struct VertexXYUV {
     GLfloat x, y, u, v;
+    void set_xyuv(GLfloat the_x, GLfloat the_y, GLfloat the_u, GLfloat the_v) {
+      x = the_x, y = the_y, u = the_u, v = the_v;
+    }
 };
 
 void DrawTileMap(const Primitive& map_primitive, ShaderUse& shader_use) {
-
+    shared_ptr<const VertexData> data = map_primitive.vertexdata();
+    shader_use.SendVertexBuffer(data->buffer().get(), VertexType::VERTEX, 0, 2, data->vertex_size());
+    shader_use.SendVertexBuffer(data->buffer().get(), VertexType::TEXTURE, 0, 2, data->vertex_size());
+    shader_use.SendTexture(0, map_primitive.texture());
+    glDrawArrays(GL_TRIANGLES, 0, 6);
 }
+
+constexpr GLfloat TILESIZE = 1.0f;
 
 } // unnamed namespace
 
@@ -42,10 +52,20 @@ TileMap::Ptr TileMap::Create(const string& name) {
     // Load tileset
     tilemap->tileset_.reset(TextureAtlas::LoadFromFile("spritesheets/"+name));
     // Create primitive
-    shared_ptr<VertexData> vertex_data(new VertexData(4u, sizeof(VertexXYUV), false));
-    tilemap->map_primitive_.reset(new Primitive(tilemap->tileset_->texture(), vertex_data));
+    shared_ptr<VertexData> data(new VertexData(6u, sizeof(VertexXYUV), false));
+    tilemap->map_primitive_.reset(new Primitive(tilemap->tileset_->texture(), data));
     // Prepare primitive
     tilemap->map_primitive_->set_drawfunction(DrawTileMap);
+    data->CheckSizes("CreatingTileMap", 6, sizeof(VertexXYUV));
+    {
+        VertexData::Mapper mapper(*data);
+        mapper.Get<VertexXYUV>(0)->set_xyuv(.0f, .0f, .0f, .0f);
+        mapper.Get<VertexXYUV>(1)->set_xyuv(.0f, 1.0f, .0f, TILESIZE);
+        mapper.Get<VertexXYUV>(2)->set_xyuv(1.0f, .0f, TILESIZE, .0f);
+        mapper.Get<VertexXYUV>(1)->set_xyuv(.0f, 1.0f, .0f, TILESIZE);
+        mapper.Get<VertexXYUV>(2)->set_xyuv(1.0f, .0f, TILESIZE, .0f);
+        mapper.Get<VertexXYUV>(5)->set_xyuv(1.0f, 1.0f, TILESIZE, TILESIZE);
+    }
     return tilemap;
 }
 
