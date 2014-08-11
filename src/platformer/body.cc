4001535c85a7aaf3ddda9a2270f6eaeaa169d7ce
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-08-11 18:48:03 -0300

    Body is draw at origin as a white quad
    Signed off by: Renato Lui Geh <renatogeh@gmail.com:

--- src/platformer/body.cc
+++ src/platformer/body.cc
@@ -24,16 +24,11 @@ namespace {
 
 struct VertexXYUV {
     GLfloat x, y, u, v;
-    void set_xyuv(GLfloat the_x, GLfloat the_y, GLfloat the_u, GLfloat the_v,
-                  const TextureAtlas::BoundPiece& piece) {
+    void set_xyuv(GLfloat the_x, GLfloat the_y, GLfloat the_u, GLfloat the_v) {
         x = the_x, y = the_y;
-        piece.ConvertToAtlas(the_u, the_v, &u, &v);
+        u = the_u, v = the_v;
     }
 };
-
-void DrawBody(const Primitive& map_primitive, ShaderUse& shader_use) {
-    // nothing
-}
 
 } // unnamed namespace
 
@@ -43,11 +38,26 @@ Body::Body(const ugdk::math::Vector2D& the_position)
 void Body::Prepare() {
     shared_ptr<VertexData> data(new VertexData(4u, sizeof(VertexXYUV), false));
     body_primitive_.reset(new Primitive(manager()->white_texture(), data));
-    body_primitive_->set_drawfunction(DrawBody);
+    VertexData::Mapper mapper(*data);
+    mapper.Get<VertexXYUV>(0)->set_xyuv(0.0f, 0.0f, 0.0f, 0.0f);
+    mapper.Get<VertexXYUV>(1)->set_xyuv(0.0f, 32.0f, 0.0f, 1.0f);
+    mapper.Get<VertexXYUV>(2)->set_xyuv(32.0f, 0.0f, 1.0f, 0.0f);
+    mapper.Get<VertexXYUV>(3)->set_xyuv(32.0f, 32.0f, 1.0f, 1.0f);
 }
 
 void Body::Render(Canvas& canvas) const {
+    ShaderUse shader_use(manager()->shaders().current_shader());
+    shared_ptr<const VertexData> data = body_primitive_->vertexdata();
 
+    shader_use.SendGeometry(canvas.current_geometry());
+    shader_use.SendEffect(canvas.current_visualeffect());
+
+    shader_use.SendTexture(0, body_primitive_->texture());
+    shader_use.SendVertexBuffer(data->buffer().get(), VertexType::VERTEX, 0, 2,
+                                data->vertex_size());
+    shader_use.SendVertexBuffer(data->buffer().get(), VertexType::TEXTURE,
+                                2*sizeof(GLfloat), 2, data->vertex_size());
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, data->num_vertices());
 }
 
 } // namespace circuit
