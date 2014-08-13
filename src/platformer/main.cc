c1eebac6a638f38aa3c5e152faaa68235184d50d
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-08-13 17:08:54 -0300

    Adds two extra bodies at random positions.

--- src/platformer/main.cc
+++ src/platformer/main.cc
@@ -2,8 +2,11 @@
 #include "tilemap.h"
 #include "body.h"
 
+#include <ctime>
 #include <functional>
 #include <memory>
+#include <random>
+#include <vector>
 #include <libjson.h>
 #include <ugdk/action/scene.h>
 #include <ugdk/graphic/canvas.h>
@@ -22,11 +25,13 @@ using ugdk::math::Vector2D;
 using ugdk::system::Configuration;
 using ugdk::system::Task;
 using std::unique_ptr;
+using std::vector;
 
 namespace {
 
 const double MAGE_SPEED = 15.0;
 const double FRAME_TIME = 1.0/60.0;
+const size_t BODY_COUNT = 2;
 double lag = 0.0;
 
 TileMap::Data data = {
@@ -78,6 +83,7 @@ Body::Space space = {
 };
 
 Body::Ptr mage;
+vector<Body::Ptr> stuff(BODY_COUNT, nullptr);
 
 TileMap::Ptr tilemap;
 
@@ -85,6 +91,8 @@ void Rendering(Canvas& canvas) {
     canvas.Clear(Color(.4, .2, .2));
     tilemap->Render(canvas);
     mage->Render(canvas);
+    for (auto& body : stuff)
+        body->Render(canvas);
 }
 
 void MoveMageTask(double dt) {
@@ -100,6 +108,15 @@ void MoveMageTask(double dt) {
     }
 }
 
+void GenerateBodies() {
+    std::default_random_engine generator(time(nullptr));
+    std::uniform_real_distribution<double> distribution(3.0,20.0);
+    for (size_t i = 0; i < BODY_COUNT; ++i) {
+        stuff[i] = Body::Create(Vector2D(distribution(generator), 2.0));
+        stuff[i]->Prepare();
+    }
+}
+
 } // unnamed namespace
 
 int main(int argc, char* argv[]) {
@@ -109,6 +126,7 @@ int main(int argc, char* argv[]) {
     ugdk::system::text_manager()->AddFont("default", "fonts/Filmcrypob.ttf", 24.0);
     ugdk::action::Scene* ourscene = new ugdk::action::Scene;
     mage = Body::Create(Vector2D(2.0, 2.0));
+    GenerateBodies();
     tilemap = TileMap::Create("sample", data);
     mage->Prepare();
     ourscene->set_render_function(Rendering);
